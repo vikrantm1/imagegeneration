ab3910ef8c79dc1c4b6d5a2ae56667959653d8c6
Vikrant Mandle <vikrantmandle12@gmail.com>
2023-02-26 11:13:00 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const path=require('path');
+const express =require('express');
+const dotenv=require('dotenv').config();
+const port=process.env.PORT || 5000;
+
+const app =express();
+//body parsers
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//Set a static folder
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/openai', require('./routes/openaiRoutes'));
+ 
+app.listen(port,()=>console.log(`App Started on ${port}`));
